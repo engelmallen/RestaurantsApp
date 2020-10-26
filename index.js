429c1ac33c7edb2f7d42f558d2919dd27a430255
Engel Mallen <emallen@uarts.edu>
2020-10-25 22:49:50 -0400

    restaurants are now being shown on the pad area

--- index.js
+++ index.js
@@ -1,78 +1,69 @@
+const search = document.getElementById('search')
+
+
+let restaurants;
+let reviews;
+
+
 const getRestaurants = async () =>{
 	const response = await fetch("http://localhost:3000/restaurants");
-	const restaurants = await response.json();
-	return restaurants;
+	restaurants = await response.json();
+	generateRestaurants(restaurants)
 };
 
-const postReview = async () =>{
+
+const generateRestaurants = (x) =>{
+	const pad = document.getElementById('pad')
+
+	// let curRestaurant = search.value
+	const mappedRestaurants = x.map((curRestaurant) => {
+		return `
+<div class="col-12 px-0 mx-0 row">	
+		<div class="restaurantTab mb-1 col row p-3 bg-light">
+
+			<div class="resPic col-2 p-0"> </div>
+			<div class="restaurantInfo col row p-0">
+				<div class=" bg-light col-12 h3 py-2"> ${curRestaurant.name} </div>
+				<div class=" bg-light col-12 py-2"> ${curRestaurant.address} </div>
+				<div class="stars col-12 starsContainer col text-center"></div>
+
+			</div>
+
+			<div class="ratings col-2 row bg-dark p-0 text-center" >
+				<input type="button" class="reviews col-12 h4 bg-info text-dark" value ="Reviews">
+				<input type="button" class="rate col-12 h4 bg-success text-dark" value ="Rate it">
+
+			</div> 
+			
+			</div>
+		</div>`
+	})
+		pad.innerHTML = mappedRestaurants.join('')
+
+};
+getRestaurants()
+
+
+
+// const postReview = async () =>{
 	
-	const newReview = {
-		restaurantId: 2,
-		stars: 1,
-		text: 'They keep rising their prices, quality is the same',
-		}
+// 	const newReview = {
+// 		restaurantId: 2,
+// 		stars: 1,
+// 		text: 'They keep rising their prices, quality is the same',
+// 		}
 
-	await fetch("http://localhost:3000/reviews", {
-		method: "POST",
-		body: JSON.stringify(newReview),
-		headers: {	
-			accept: 'application/json', 
-					'Content-Type': "application/json",
-				},
+// 	await fetch("http://localhost:3000/reviews", {
+// 		method: "POST",
+// 		body: JSON.stringify(newReview),
+// 		headers: {	
+// 			accept: 'application/json', 
+// 					'Content-Type': "application/json",
+// 				},
 
-	});
-};
+// 	});
+// };
 
-/*
 
-Pre-requisites: install JSON server: https://github.com/typicode/json-server#getting-started
 
-Create a web application that shows a list of restaurants from your database and their reviews. 
 
-Core requirements:
-
-1.  Populate the page with restaurants from your local database, 
-    showing the name, address, an image, and the average review score. 
-    For instance, if there are 2 reviews on a restaurant, one for 
-    4 stars and one for 3 stars, the average review score should be 3.5. Use .map().
-
-2.  Show the reviews from your local database below each restaurant, 
-    showing the text and star rating. Use .filter() and .map().
-
-3. Sort the restaurants from highest-rated to lowest-rated. Use .sort().
-
-4.  Create a form that allows the user to write a new review and give 
-    a star rating. When the user submits the form the new review should 
-    show up on the page WITHOUT reloading the page.
-
-    Complete at least one of the following requirements then complete as many 
-    others as possible in whichever order you'd like.
-
-1.  The form should not be shown until someone clicks a button. 
-    Make sure the correct restaurantId is being added to the review. 
-    Only one form should be visible at a time.
-
-2.  The reviews should only be shown below the restaurant after 
-    the user clicks a restaurant. Only one restaurant's reviews 
-    should be visible at a time.
-
-3.  Add the option to delete reviews, but not restaurants. 
-    Deleting a review should remove it from the database. 
-    After deleting, show the updated list of reviews WITHOUT reloading the page.
-
-4. 	Add users to the database. Associate them with reviews and show the user's details on the review. 
-	Use the .find() method to do so.
-
-    Grading criteria:
-
-1.  Use the methods that are listed in each requirement.
-2.  Use async/await and fetch and use only the data that exists in your database.
-3.  Use only ES6+ techniques if needed: template strings (`${}`), arrow functions, forEach(), etc.
-4.  Use GitHub and make a new branch for each numbered task, so at least 5 branches total. 
-    Give the branch a meaningful name using this convention: feature/show-restaurants. 
-    Merge the feature branch into your main branch when you complete the task.
-5.  Do not have any extraneous comments beyond explanations of code (if needed) 
-    in your final result. Do not have any console.logs, in comments or otherwise.
-6.  CSS is not necessary and will not be graded but is encouraged for the sake of practice.
-
-*bx n
