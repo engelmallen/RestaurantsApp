29426e0d8b66c04e32a5e85c5c6ea43986a43cab
Engel Mallen <emallen@uarts.edu>
2020-11-01 14:14:43 -0500

    Reviews sorted by stars

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ const starBtns = document.getElementsByClassName('starBtns')[0]
 
 let restaurants;
 let reviews;
-// let currentStart = 0;
 let starCounter = 0;
 
 
 const getRestaurants = async () =>{
 	const response = await fetch("http://localhost:3000/restaurants");
 	restaurants = await response.json();
+	}
 
-	// generateRestaurants(restaurants)
-}
 const getReviews = async () =>{
 	const response = await fetch("http://localhost:3000/reviews");
 	reviews = await response.json();
-
-	// generateRestaurants(restaurants)
-}
+	}
 
 getRestaurants()
 getReviews()
@@ -34,46 +30,80 @@ const searchCriteria = (e) =>{
 	generateRestaurants(filteredRestaurants)
 	}
 
+const sortIt = ( a, b ) =>{
+  if ( a.stars < b.stars ){
+    return 1;
+  }
+  if ( a.stars > b.stars ){
+    return -1;
+  }
+  return 0;
+}
+
 const searchByRate =  (filtered) =>{
-	//  console.log(filtered)
 	starCounter = 0; 
-	filtered.forEach((f)=>{
+	filtered.sort(sortIt).forEach((f)=>{
 		return starCounter += f.stars
 		}) 
 		starCounter /= filtered.length
 		starCounter = Math.round(starCounter)
-		console.log(starCounter)
+		// console.log(starCounter)
 		return starCounter
 	}
 
+const generateReviews = (curReview) =>{
+	var mappedReviews = curReview.map((mapped) =>{
+		return `
+		<div class="custumerReview border-bottom border-top-1 col-12 bg-white py-2">
+					<div class="userName h6 col font-weight-bold px-2"> Mr.User </div>
+					<div class="stars col-4" style="background: url('images/img${mapped.stars}stars.png') no-repeat; background-size: contain"></div>	
+		
+					<div class="userRevew col-12 border-top-1" > 
+						${mapped.text}
+					</div>
+		
+				</div>`
+
+	})
+	return mappedReviews.join('')
+	console.log(mappedReviews)
+}
+
 const generateRestaurants = (x) =>{
 	const pad = document.getElementById('pad')
-	const mappedRestaurants = x.map((curRestaurant) => {
+	const mappedRestaurants = x.map((curRestaurant,curReviewer) => {
 	
 		let getThisReview = reviews.filter((curId)=>{
 		return curRestaurant.id === curId.restaurantId
 	})
 	// console.log(getThisReview)
 	searchByRate(getThisReview)
+	curReviewer = generateReviews(getThisReview)
+	
 
 		return `
-<div class="col-12 px-0 mx-0 row">	
-		<div class="restaurantTab mb-1 col row p-3 bg-light">
+<div class="col-12 px-0 mx-0 mb-1 row">	
+		<div class="restaurantTab col-12 w-100 row m-0 p-3 bg-light">
 
-			<div class="resPic col-2 p-0" style="background: url('${curRestaurant.imgUrl}') no-repeat; background-size: contain"> </div>
-			<div class="restaurantInfo col row p-0">
+			<div class="resPic col-2 p-0 ">
+			<img src="${curRestaurant.imgUrl}" width="100%">
+			</div>
+			<div class="restaurantInfo col row px-5">
 				<div class=" bg-light col-12 h3 py-2"> ${curRestaurant.name} </div>
-				<div class=" bg-light col-12 py-2"> ${curRestaurant.address} </div>
+				<div class=" bg-light col-12 py-2 pl-0"> ${curRestaurant.address} </div>
 				<div class="stars col-12 starsContainer text-center" style="background: url('images/img${starCounter}stars.png') no-repeat; background-size: contain"></div>
 			</div>
 
-			<div class="ratings col-2 row bg-dark p-0 text-center" >
+			<div class="ratings col-2 row py-2 text-center" >
 				<input type="button" class="reviews col-12 h4 bg-info text-dark" value ="Reviews">
 				<input type="button" class="rate col-12 h4 bg-success text-dark" value ="Rate it">
-			</div> 
 			</div>
 			
+			</div>
+			<div class="row col-12 w-100 m-0 p-0"> ${curReviewer} </div> 
 		</div>`
+
+
 	})
 
 		pad.innerHTML = mappedRestaurants.join('')
